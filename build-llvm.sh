ea58e3ca2225fae3310dab64ee069d0023845e4c
Pericles Alves <periclesroalves@gmail.com>
2014-09-04 16:44:06 -0300

    Initial push from LLVM SVN trunk.

---
+++ build-llvm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Configure w/ assert, wo/ optmize.
+# Dist files are stored under build/Debug+Asserts
+# NOTE: other build can be put under this same build directory.
+/Users/periclesalves/ufmg-research/llvm/configure --prefix=/Users/periclesalves/ufmg-research/dist/ --enable-libcpp=yes
