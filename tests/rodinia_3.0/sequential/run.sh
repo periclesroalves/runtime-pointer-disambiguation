92291c53bc7274a6bd0d303966d2bd75e098adf5
Pericles Alves <periclesroalves@gmail.com>
2015-01-30 18:07:14 -0200

    Adding new benchmarks to Rodinia sequential

--- tests/rodinia_3.0/sequential/run.sh
+++ tests/rodinia_3.0/sequential/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Change these to reflect you compilation command (executable + flags).
 export CC=gcc
 export CXX=g++
 
-DIR_LIST=(b+tree backprop bfs heartwall hotspot kmeans)
+DIR_LIST=(b+tree backprop bfs heartwall hotspot kmeans lavaMD particlefilter pathfinder streamcluster)
 
 # Build all benchmarks.
 for dir in "${DIR_LIST[@]}"
@@ -27,6 +28,12 @@ for i in {1..10}; do time ./bfs/bfs 1 ../data/bfs/graph1MW_6.txt > /dev/null; do
 echo $'\nhotspot'
 for i in {1..10}; do time ./hotspot/hotspot 512 512 2 1 ../data/hotspot/temp_512 ../data/hotspot/power_512 > /dev/null; done
 
+echo $'\nparticlefilter'
+for i in {1..10}; do time ./particlefilter/particle_filter -x 128 -y 128 -z 10 -np 10000 > /dev/null; done
+
+echo $'\npathfinder'
+for i in {1..10}; do time ./pathfinder/pathfinder 100000 100 > /dev/null; done
+
 
 # Run larger benchmarks twice.
 echo $'\nheartwall'
@@ -34,6 +41,12 @@ for i in {1..2}; do time ./heartwall/heartwall ../data/heartwall/test.avi 20 1 >
 
 echo $'\nkmeans'
 for i in {1..2}; do time ./kmeans/kmeans -i ../data/kmeans/kdd_cup > /dev/null; done
+
+echo $'\nlavaMD'
+for i in {1..2}; do time ./lavaMD/lavaMD -cores 1 -boxes1d 10 > /dev/null; done
+
+echo $'\nstreamcluster'
+for i in {1..2}; do time ./streamcluster/sc_omp 10 20 256 65536 65536 1000 none output.txt 1 > /dev/null 2> /dev/null; done
 
 
 # Clean all benchmarks.
