2ac530ad2a5e6178f8e8999fbdb8d6151e1de15b
Fabian Gruber <fabian.gruber@fadeopolis.com>
2015-02-27 11:04:14 +0100

    Made sure warmup allocations in HybridAlias benchmarks are not eliminated by compiler.

--- HybridAlias/benchmark/benchmark.cpp
+++ HybridAlias/benchmark/benchmark.cpp
@@ -4,13 +4,22 @@
 */
 
 #include <benchmark/benchmark.h>
+#include <cassert>
+
+// prevent compiler from optimizing away useless malloc calls
+extern "C" __attribute__((noinline))
+void *xmalloc(size_t size) {
+	assert(size);
+	void *ptr = malloc(size);
+	assert(ptr);
+	return ptr;
+}
 
 int main(int argc, const char **argv, const char **envp) {
 	// put some pressure on the shadow-malloc
-	for (int i = 0; i < 5000; i++) {
-		for (int j = 0; j < 100; j++)
-			malloc(i);
-	}
+	for (int i = 0; i < 5000; i++)
+		for (int j = 1; j < 256; j++)
+			(void) xmalloc(j);
 
 	int         my_argc   = 3;
 	const char *my_argv[] = {
